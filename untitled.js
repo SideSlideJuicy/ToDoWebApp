3731fe7ba1d4ff38152425a5116cdcf60153df38
Jussi Nevalainen <jussinevalainen@Jussis-Laptop.local>
2023-04-10 11:42:00 +0300

    start to clean js code

--- untitled.js
+++ untitled.js
@@ -72,12 +72,63 @@ document.getElementById("date").innerHTML = d + "/" + m + "/" + y;
 
 
 
-function newTask(){
-    document.getElementById("add-new-task").classList.toggle("active");
+Scroll = (
+    function(){
+          var x,y;
+         function hndlr(){
+            window.scrollTo(x,y);
+            //return;
+          }  
+          return {
+
+               disable : function(x1,y1){
+                    x = x1;
+                    y = y1;
+                   if(window.addEventListener){
+                       window.addEventListener("scroll",hndlr);
+                   } 
+                   else{
+                        window.attachEvent("onscroll", hndlr);
+                   }     
+
+               },
+               enable: function(){
+                      if(window.removeEventListener){
+                         window.removeEventListener("scroll",hndlr);
+                      }
+                      else{
+                        window.detachEvent("onscroll", hndlr);
+                      }
+               } 
+
+          }
+    })();
+
+
+
+function newTaskButton(){
+    document.getElementById("new-task").classList.toggle("active");
+    Scroll.disable(0,document.body.scrollTop);
+
     document.getElementById('task-name').value = "";
     document.getElementById('task-description').value = "";
     document.getElementById("add-btn").disabled = true; 
+    document.getElementById('date').checked = false;
+    document.getElementById('time').checked = false;
+    document.getElementById('repeat').checked = false;
+    document.getElementById('priority').checked = false;
+    document.getElementById('notifications').checked = false;
+
+
+        
+    
+
 }
+
+
+        
+
+        
 
 
 function btnActivation(){
@@ -88,4 +139,100 @@ function btnActivation(){
         document.getElementById("add-btn").disabled = false;
 
     }           
+}
+
+function addTaskButton(){
+    document.getElementById("new-task").classList.toggle("active");
+
+    document.getElementById('task-name').value = "";
+    document.getElementById('task-description').value = "";
+    document.getElementById("add-btn").disabled = true; 
+    Scroll.enable();
+}
+
+function cancelTaskButton(){
+    document.getElementById("new-task").classList.toggle("active");
+
+    document.getElementById('task-name').value = "";
+    document.getElementById('task-description').value = "";
+    document.getElementById("add-btn").disabled = true; 
+    Scroll.enable();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function date(){
+    document.getElementById('time').checked = false;
+    document.getElementById('repeat').checked = false;
+    document.getElementById('priority').checked = false;
+    document.getElementById('notifications').checked = false;
+}
+
+function time(){
+    document.getElementById('date').checked = false;
+    document.getElementById('repeat').checked = false;
+    document.getElementById('priority').checked = false;
+    document.getElementById('notifications').checked = false;
+}
+
+function repeat(){
+    document.getElementById('time').checked = false;
+    document.getElementById('date').checked = false;
+    document.getElementById('priority').checked = false;
+    document.getElementById('notifications').checked = false;
+}
+
+function priority(){
+    document.getElementById('time').checked = false;
+    document.getElementById('repeat').checked = false;
+    document.getElementById('date').checked = false;
+    document.getElementById('notifications').checked = false;
+}
+
+function notifications(){
+    document.getElementById('time').checked = false;
+    document.getElementById('repeat').checked = false;
+    document.getElementById('priority').checked = false;
+    document.getElementById('date').checked = false;
+}
+
+
+
+
+
+
+
+function preventScroll(e){
+    e.preventDefault();
+    e.stopPropagation();
+
+    return false;
+}
+
+var disableBodyScroll = function(){
+    
+}
+
+
+
+
+
+
+
+
+
+
+function clearNewTaskContent(){
+
 }
